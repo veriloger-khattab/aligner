6f92fc0f1122bdd6d0950367d0fc9d84b74ba1f9
Ahmad Khattab <ahmadkhattab.w@gmail.com>
2025-08-12 14:52:26 +0300

    modified the batch script to stop the simulation if one error was encountered

--- scripts/run_batch.bat
+++ scripts/run_batch.bat
@@ -1,19 +1,19 @@
 @echo off
-echo ______________________________________________________________
-echo                        Ahmad Khattab
-echo ==============================================================
-echo   Compiling the files, Running UVM without opening Questasim
-echo   ==========================================================
+echo      __________________________________________________________________
+echo                                 Ahmad Khattab
+echo        ==============================================================
+echo          compiling the files, running REG_ACCESS_TEST in batch mode
+echo          ==========================================================
 echo.
-echo                         .         .
-echo                       .    .   .    .
-echo                      .       .       .
-echo                       .             .
-echo                          .      .
-echo                              .
+echo                                 .         .
+echo                               .    .   .    .
+echo                              .       .       .
+echo                                .           .
+echo                                  .      .
+echo                                      .
 echo.
 qrun -batch -access=rw+/. -uvmhome uvm-1.2 -mfcu ^
-Design\design.sv Testbench\testbench.sv ^
+design\\design.sv testbench\\testbench.sv ^
 -voptargs="+acc=npr" ^
--do "vlog -timescale 1ns/1ns +incdir+Testbench Design\design.sv Testbench\testbench.sv; run -all; exit" ^
-+UVM_TESTNAME=cfs_algn_test_reg_access
+-do "vlog -timescale 1ns/1ns +incdir+testbench design\\design.sv testbench\\testbench.sv; run -all; exit" ^
++UVM_TESTNAME=cfs_algn_test_reg_access +UVM_MAX_QUIT_COUNT=1
