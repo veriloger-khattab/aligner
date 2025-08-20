c2fc4ff5fe70ef252f9b72529a64853dfcf79bd4
Ahmad Khattab <ahmadkhattab.w@gmail.com>
2025-08-21 00:38:58 +0300

    added apb monitoring item class to monitor the apb signals coming from the aligner dut

---
+++ testbench/cfs_apb_item_mon.sv
@@ -0,0 +1,133 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////
+// Author    : Ahmad Khattab
+// Date      : 8/5/25
+// File      : cfs_apb_item_mon.sv
+// Status    : finished
+// Goal      : creating a transaction object that contains monitoring infomation about apb transfer
+// Instructor: Cristian Slav
+// Tips      : read the code documentation below to understand how the code works
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+`ifndef CFS_APB_ITEM_MON_SV
+  `define CFS_APB_ITEM_MON_SV
+
+  class cfs_apb_item_mon extends cfs_apb_item_base;
+
+    cfs_apb_response response;
+    int unsigned length;
+    int unsigned prev_item_delay;
+
+    `uvm_object_utils(cfs_apb_item_mon)                                                                                                              // APB monitor item is now registered with uvm factory & can use core utility methods & features
+
+     virtual function string convert2string();
+      string result = super.convert2string();
+
+      result = $sformatf("%0s, data:%0x response: %0s, length: %0d, prev item delay: %0d", result, data, response.name(), length, prev_item_delay);
+      return result;
+    endfunction
+
+    function new(string name = "");                                                                                                                  // Mandatory code for uvm objects (declaration of constructor)
+      super.new(name);
+    endfunction
+
+  endclass
+`endif
+
+
+
+
+
+
+
+
+//////////////////////////////////////////////////////ENABLE DOCS BY REMOVING "/" IN THE NEXT LINE//////////////////////////////////////////////////
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                         --- "Implementation steps" ---                                                          *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                                                                                                                 *
+ *"  1- declare apb monitor item class and extend it from apb base item                                                                           "*
+ *"  2- write mandatory code                                                                                                                      "*
+ *"  3- declare response, length and prev item delay to monitor transaction properties. remember: common fields are declared inside apb_item_base "*
+ *"  4- override convert2string() function to print data, response, transaction length and previous item delay                                    "*
+ *                                                                                                                                                 *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+
+
+
+//////////////////////////////////////////////////////ENABLE DOCS BY REMOVING "/" IN THE NEXT LINE//////////////////////////////////////////////////
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                               --- "Merge info" ---                                                              *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                                                                                                                 *
+ *"   1- include apb monitor item inside apb package                                                                                              "*
+ *"   2- create an instance of apb item mon inside collect transactions task inside apb monitor                                                   "*
+ *                                                                                                                                                 *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+
+
+
+//////////////////////////////////////////////////////ENABLE DOCS BY REMOVING "/" IN THE NEXT LINE//////////////////////////////////////////////////
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                            --- "Diagarm Hierarchy" ---                                                          *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                                                                                                                 *
+ *"   testbench                                                                                                                                   "*
+ *"            tests                                                                                                                              "*
+ *"                 environment                                                                                                                   "*
+ *"                            config                                                                                                             "*
+ *"                            virtual_sequencer                                                                                                  "*
+ *"                            scoreboard                                                                                                         "*
+ *"                            coverage                                                                                                           "*
+ *"                            model                                                                                                              "*
+ *"                                 register_model                                                                                                "*
+ *"                            predictor                                                                                                          "*
+ *"                            rx_agent                                                                                                           "*
+ *"                                    config                                                                                                     "*
+ *"                                    coverage                                                                                                   "*
+ *"                                    sequencer                                                                                                  "*
+ *"                                    driver                                                                                                     "*
+ *"                                    monitor                                                                                                    "*
+ *"                                    interface                                                                                                  "*
+ *"                            tx_agent                                                                                                           "*
+ *"                                    config                                                                                                     "*
+ *"                                    coverage                                                                                                   "*
+ *"                                    sequencer                                                                                                  "*
+ *"                                    driver                                                                                                     "*
+ *"                                    monitor                                                                                                    "*
+ *"                                    interface                                                                                                  "*
+ *"                                                                                                                                               "*
+ *"            apb_if.sv       macros.svh                                                                                                         "*
+ *"                ↓           ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~                          "*
+ *"                ↓           | apb_pkg                                                                               |                          "*
+ *"                ↓           ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~                          "*
+ *"                ↓set        | uvm_pkg::*                                                                            |                          "*
+ *"         -----------------  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~                          "*
+ *"         | uvm_config_db |  |                                                                                       |                          "*
+ *"         |               |  |                             ~--- cfs_apb_sequence_random<<<                           |                          "*
+ *"         |    apb_vif    |  |                             |                             ^                           |                          "*
+ *"         -----------------  |     cfs_apb_sequence_base <-~--- cfs_apb_sequence_rw      ^                       <   |                          "*
+ *"                ↓           |                   ↑  ^      |                             ^                       ^   |                          "*
+ *"                ↓           |                   ↑  ^      ~--- cfs_apb_sequence_simple>>>                       ^   |                          "*
+ *"                ↓       get |                   ↑  ^                        ^                                   ^   |                          "*
+ *"                 → → → → → →|   > cfs_apb_agent ↑  <<<<<<<<<<<<<<<<<<<<<<<<<^                    <              ^   |                          "*
+ *"                            |   ^              cfs_apb_sequencer           <^                   <^              ^   |                          "*
+ *"                            |   ^>      >      cfs_apb_driver              <^                   <^              ^   |                          "*
+ *"                            |   ^>      ^>     cfs_apb_coverage             ^       <           <^              ^   |                          "*
+ *"                            |   ^>      ^>     cfs_apb_monitor              ^      <^           <^              ^   |                          "*
+ *"                            |   ^>      <<<<<<<cfs_apb_agent_config         ^       ^            ^              ^   |                          "*
+ *"                            |   ^                                           ^       ^            ^              ^   |                          "*
+ *"                            |   ^                     ~-- cfs_apb_item_drv>>>       ^            ^              ^   |                          "*
+ *"                            |   ^                     |                             ^            ^              ^   |                          "*
+ *"                            |   ^>cfs_apb_item_base <-~                             ^            ^              ^   |                          "*
+ *"                            |   ^                     |                             ^            ^              ^   |                          "*
+ *"                            |   ^                     ~-- cfs_apb_item_mon>>>>>>>>>>>            ^              ^   |                  (o)     "*
+ *"                            |   ^                         ^                                      ^              ^   |                          "*
+ *"                            |   ^                         ^                              cfs_apb_reset_handler  ^   |                          "*
+ *"                            |   ^<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<cfs_apb_types>>>>>>>>>>>   |                          "*
+ *"                            |                                                                                       |                          "*
+ *"                            ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~                          "*
+ *"            dut                                                                                                                                "*
+ *                                                                                                                                                 *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
