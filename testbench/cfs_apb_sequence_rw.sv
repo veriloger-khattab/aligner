9ad0dbe936d1db025b26853e63e95fc0a00d28d1
Ahmad Khattab <ahmadkhattab.w@gmail.com>
2025-08-26 09:05:05 +0300

    created a read write sequence for apb protocol

---
+++ testbench/cfs_apb_sequence_rw.sv
@@ -0,0 +1,140 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////
+// Author    : Ahmad Khattab
+// Date      : 8/5/25
+// File      : cfs_apb_sequence_rw.sv
+// Status    : finished
+// Goal      : creating a sequence that reads and writes apb transactions
+// Instructor: Cristian Slav
+// Tips      : read the code documentation below to understand how the code works
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+`ifndef CFS_APB_SEQUENCE_RW_SV
+  `define CFS_APB_SEQUENCE_RW_SV
+
+  class cfs_apb_sequence_rw extends cfs_apb_sequence_base;
+    
+    rand cfs_apb_addr addr; 
+    rand cfs_apb_data wr_data;
+
+    `uvm_object_utils(cfs_apb_sequence_rw)                                                                                                           // APB read/write sequence is now registered with uvm factory & can use core utility methods & features
+
+    function new(string name = "");                                                                                                                  // Mandatory code for uvm objects (declaration of constructor)
+      super.new(name);
+    endfunction
+
+    virtual task body();
+    cfs_apb_item_drv item;
+
+    `uvm_do_with(item, {
+      dir  == CFS_APB_READ;
+      addr == local::addr;                                                                                                                           // Forcing the simulator to take the address from the sequence
+      });
+
+    `uvm_do_with(item, {
+      dir  == CFS_APB_WRITE;
+      addr == local::addr;
+      data == wr_data;
+      });
+    endtask
+
+  endclass   
+`endif
+
+
+
+
+
+
+
+
+//////////////////////////////////////////////////////ENABLE DOCS BY REMOVING "/" IN THE NEXT LINE//////////////////////////////////////////////////
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                         --- "Implementation steps" ---                                                          *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                                                                                                                 *
+ *"   1- extend apb read write sequence from apb base sequence                                                                                    "*
+ *"   2- write mandatory code for uvm objects                                                                                                     "*
+ *"   3- ranodmize address and write data that will be sent to the sequencer                                                                      "*
+ *"   4- replace item creation, start_item(item) and finish_item(item) with `uvm_do_with(item){constraints}                                       "*
+ *                                                                                                                                                 *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+
+
+
+//////////////////////////////////////////////////////ENABLE DOCS BY REMOVING "/" IN THE NEXT LINE//////////////////////////////////////////////////
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                               --- "Merge info" ---                                                              *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                                                                                                                 *
+ *"   1- include apb read write sequence inside apb package                                                                                       "*
+ *"   2- create an instance of apb read write sequence in register access test inside run phase                                                   "*
+ *                                                                                                                                                 *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+
+
+
+//////////////////////////////////////////////////////ENABLE DOCS BY REMOVING "/" IN THE NEXT LINE//////////////////////////////////////////////////
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                            --- "Diagarm Hierarchy" ---                                                          *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                                                                                                                 *
+ *"   testbench                                                                                                                                   "*
+ *"            tests                                                                                                                              "*
+ *"                 environment                                                                                                                   "*
+ *"                            config                                                                                                             "*
+ *"                            virtual_sequencer                                                                                                  "*
+ *"                            scoreboard                                                                                                         "*
+ *"                            coverage                                                                                                           "*
+ *"                            model                                                                                                              "*
+ *"                                 register_model                                                                                                "*
+ *"                            predictor                                                                                                          "*
+ *"                            rx_agent                                                                                                           "*
+ *"                                    config                                                                                                     "*
+ *"                                    coverage                                                                                                   "*
+ *"                                    sequencer                                                                                                  "*
+ *"                                    driver                                                                                                     "*
+ *"                                    monitor                                                                                                    "*
+ *"                                    interface                                                                                                  "*
+ *"                            tx_agent                                                                                                           "*
+ *"                                    config                                                                                                     "*
+ *"                                    coverage                                                                                                   "*
+ *"                                    sequencer                                                                                                  "*
+ *"                                    driver                                                                                                     "*
+ *"                                    monitor                                                                                                    "*
+ *"                                    interface                                                                                                  "*
+ *"                                                                                                                                               "*
+ *"            apb_if.sv       macros.svh                                                                                                         "*
+ *"                ↓           ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~                          "*
+ *"                ↓           | apb_pkg                                                                               |                          "*
+ *"                ↓           ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~                          "*
+ *"                ↓set        | uvm_pkg::*                                                                            |                          "*
+ *"         -----------------  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~                          "*
+ *"         | uvm_config_db |  |                                                                                       |                          "*
+ *"         |               |  |                             ~--- cfs_apb_sequence_random<<<                           |                          "*
+ *"         |    apb_vif    |  |                             |                             ^                           |                          "*
+ *"         -----------------  |     cfs_apb_sequence_base <-~--- cfs_apb_sequence_rw      ^                       <   |                  (o)     "*
+ *"                ↓           |                   ↑  ^      |                             ^                       ^   |                          "*
+ *"                ↓           |                   ↑  ^      ~--- cfs_apb_sequence_simple>>>                       ^   |                          "*
+ *"                ↓       get |                   ↑  ^                        ^                                   ^   |                          "*
+ *"                 → → → → → →|   > cfs_apb_agent ↑  <<<<<<<<<<<<<<<<<<<<<<<<<^                    <              ^   |                          "*
+ *"                            |   ^              cfs_apb_sequencer           <^                   <^              ^   |                          "*
+ *"                            |   ^>      >      cfs_apb_driver              <^                   <^              ^   |                          "*
+ *"                            |   ^>      ^>     cfs_apb_coverage             ^       <           <^              ^   |                          "*
+ *"                            |   ^>      ^>     cfs_apb_monitor              ^      <^           <^              ^   |                          "*
+ *"                            |   ^>      <<<<<<<cfs_apb_agent_config         ^       ^            ^              ^   |                          "*
+ *"                            |   ^                                           ^       ^            ^              ^   |                          "*
+ *"                            |   ^                     ~-- cfs_apb_item_drv>>>       ^            ^              ^   |                          "*
+ *"                            |   ^                     |                             ^            ^              ^   |                          "*
+ *"                            |   ^>cfs_apb_item_base <-~                             ^            ^              ^   |                          "*
+ *"                            |   ^                     |                             ^            ^              ^   |                          "*
+ *"                            |   ^                     ~-- cfs_apb_item_mon>>>>>>>>>>>            ^              ^   |                          "*
+ *"                            |   ^                         ^                                      ^              ^   |                          "*
+ *"                            |   ^                         ^                              cfs_apb_reset_handler  ^   |                          "*
+ *"                            |   ^<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<cfs_apb_types>>>>>>>>>>>   |                          "*
+ *"                            |                                                                                       |                          "*
+ *"                            ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~                          "*
+ *"            dut                                                                                                                                "*
+ *                                                                                                                                                 *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
