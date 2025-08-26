2f90a6de7c8f4d2151471ee9101fd1c88a6a3f7d
Ahmad Khattab <ahmadkhattab.w@gmail.com>
2025-08-26 09:23:40 +0300

    simple, read write and random apb sequences now run in parallel inside reg acess test

--- testbench/cfs_algn_test_reg_access.sv
+++ testbench/cfs_algn_test_reg_access.sv
@@ -1,30 +1,86 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////
 // Author    : Ahmad Khattab
-// Date      : 8/8/25
+// Date      : 7/8/25
 // File      : cfs_algn_test_reg_access.sv
-// Status    : In progress
-// Goal      : Testing accessing aligner's registers
+// Status    : not finalized
+// Goal      : testing accessing aligner's registers
 // Instructor: Cristian Slav
-// Tips      : Read the code guide to understand how the code works
+// Tips      : read the code documentation below to understand how the code works
 //////////////////////////////////////////////////////////////////////////////////////////////////
 
 `ifndef CFS_ALGN_TEST_REG_ACCESS_SV
   `define CFS_ALGN_TEST_REG_ACCESS_SV
 
-  class cfs_algn_test_reg_access extends cfs_algn_test_base;                                       // reg access test inherit from base test
+  class cfs_algn_test_reg_access extends cfs_algn_test_base;                                                                                         // Reg access test inherit from base test
 
-    `uvm_component_utils(cfs_algn_test_reg_access)                                                 // This enables access to uvm features
+    `uvm_component_utils(cfs_algn_test_reg_access)                                                                                                   // Aligner register access test is now registered with uvm factory & can use all utility methods & features
 
-    function new(string name = "", uvm_component parent);
+    function new(string name = "", uvm_component parent);                                                                                            // Mandatory code for uvm components (declaration of constructor)
       super.new(name, parent);
     endfunction
 
-    virtual task run_phase(uvm_phase phase);
-      phase.raise_objection(this, "TEST START");
-      `uvm_info("DEBUG", "start of test", UVM_LOW)
-        #100ns;
+    virtual task run_phase(uvm_phase phase);                                                                                                         // Simulation stops when the objection counter drops to 0
+      phase.raise_objection(this, "TEST_DONE");                                                                                                      // It acts as an up counter
+      #(100ns);
+
+
+      fork                                                                                                                                           // Now all the sequences are running in parallel. Hence, the test is more random
+        begin                                                                                                                                        // This tests what happen to the components and signals when reset is applied
+          cfs_apb_vif vif = env.apb_agent.agent_config.get_vif();
+          repeat(3) begin
+            @(posedge vif.psel);
+          end
+          #11ns;
+          vif.preset_n <= 0;
+          repeat(4) begin
+            @(posedge vif.pclk);
+          end
+          vif.preset_n <= 1;
+        end
+        begin
+          cfs_apb_sequence_simple seq_simple = cfs_apb_sequence_simple::type_id::create("seq_simple");
+          void'(seq_simple.randomize() with{
+            item.addr == 'h0;                                                                                                                        // Constraining the simple sequence to access the control register
+            item.dir  == CFS_APB_WRITE;
+            item.data == 'h11;
+            });
+
+          seq_simple.start(env.apb_agent.sequencer);
+        end
+
+        begin
+          cfs_apb_sequence_rw seq_rw = cfs_apb_sequence_rw::type_id::create("seq_rw");
+          void'(seq_rw.randomize() with{
+            addr == 'hC;                                                                                                                             // Constraining the read write sequence to access the status register (writing returns p slave error)
+            });
+
+          seq_rw.start(env.apb_agent.sequencer);
+
+        end
+
+        begin
+          cfs_apb_sequence_random seq_random = cfs_apb_sequence_random::type_id::create("seq_random");
+          void'(seq_random.randomize() with{
+            num_items == 3;
+            });
+
+          seq_random.start(env.apb_agent.sequencer);
+        end
+      join
+
+      begin
+        cfs_apb_sequence_random seq_random = cfs_apb_sequence_random::type_id::create("seq_random");
+        void'(seq_random.randomize() with{
+          num_items == 3;
+        });
+
+       seq_random.start(env.apb_agent.sequencer);
+      end
+
+      #(100ns);
+
       `uvm_info("DEBUG", "end of test", UVM_LOW)
-      phase.drop_objection(this, "TEST DONE");
+      phase.drop_objection(this, "TEST_DONE");                                                     // It acts as a down counter
     endtask
 
   endclass
@@ -51,9 +107,15 @@
  *                                                         --- "Implementation steps" ---                                                          *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  *                                                                                                                                                 *
- *"  1- Start by extending reg access test from base test                                                                                         "*
- *"  2- Write mandatory code                                                                                                                      "*
- *"  3- write the run_phase() virtual task and this is what will run during the simulation starting from raising objection to dropping it         "*
+ *"   1- declare aligner register access test class and extend it from alinger base test                                                          "*
+ *"   2- Write mandatory code                                                                                                                     "*
+ *"   3- implement virtual task run_phase() and this is what will run during the simulation starting from raising objection to dropping it        "*
+ *"   4- create an instance of apb simple sequence in register access test inside run phase                                                       "*
+ *"   5- start the simple sequence by sending it to the sequencer                                                                                 "*
+ *"   6- create an instance of apb read write sequence in register access test inside run phase                                                   "*
+ *"   7- start the read write sequence by sending it to the sequencer                                                                             "*
+ *"   8- create an instance of apb random sequence in register access test inside run phase                                                       "*
+ *"   9- start the random sequence by sending 3 simple sequences to the sequencer                                                                 "*
  *                                                                                                                                                 *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
@@ -65,10 +127,27 @@
  *                                                               --- "Merge info" ---                                                              *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  *                                                                                                                                                 *
- *"  1- Register aligner register access test with the uvm factory to enable access to uvm features                                               "*
- *      `uvm_component_utils(cfs_algn_test_reg_access)                                                                                             *
+ *"   1- include register access test files inside the test package                                                                               "*
  *                                                                                                                                                 *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+
+
+//////////////////////////////////////////////////////ENABLE DOCS BY REMOVING "/" IN THE NEXT LINE//////////////////////////////////////////////////
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                        --- "Test inheritance tree" ---                                                          *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                                                                                                                 *
+ *"                                                                                                                                               "*
+ *"                                     ~--- cfs_algn_test_random                                                                                 "*
+ *"                                     |                                                                                                         "*
+ *"   uvm_test <- cfs_algn_test_base <--~--- cfs_algn_test_reg_access                                             We are here now         (o)     "*
+ *"                                     |                                                                                                         "*
+ *"                                     ~--- cfs_algn_test_illegal_rx                                                                             "*
+ *"                                                                                                                                               "*
+ *                                                                                                                                                 *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 
 
 
@@ -78,9 +157,9 @@
  *                                                            --- "Diagarm Hierarchy" ---                                                          *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  *                                                                                                                                                 *
- *"   testbench                                                                                                                           (o)     "*
+ *"   testbench                                                                                                                                   "*
  *"            tests                                                                         reg access        <- We are here now         (o)     "*
- *"                 environment                                                                                                           (o)     "*
+ *"                 environment                                                                                                                   "*
  *"                            config                                                                                                             "*
  *"                            virtual_sequencer                                                                                                  "*
  *"                            scoreboard                                                                                                         "*
